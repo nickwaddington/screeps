2e58de43e92947f34f30faadafe2f494b6dc301e
nickwaddington <nicolaaswaddington@gmail.com>
2017-03-04 15:56:06 +1300

    Starting template

--- main.js
+++ main.js
@@ -1,1 +1,26 @@
-console.log('Test')
+module.exports.loop = function () {
+    var timeCurrent = Game.time;
+    
+    //clear dead creeps memory
+    for(var name in Memory.creeps) {
+        if(!Game.creeps[name]) {
+            delete Memory.creeps[name];
+        }
+    }
+    
+    for(var currentRoom in Game.rooms) {
+        var rm = Game.rooms[currentRoom];
+        
+        
+    }
+    
+    for(var currentCreep in Game.creeps) {
+        var crp = Game.creeps[currentCreep];
+        
+        
+    }
+    
+    //print time an cpu usage
+    var cpuUsedThisTick = Game.cpu.getUsed()*100/Game.cpu.limit;
+    console.log("Time: " + timeCurrent + "  " + Math.round(cpuUsedThisTick) + "% CPU used");
+};
