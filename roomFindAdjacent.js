15b2981e727c0d5edac8304eef1c9b6fbcc457c3
nickwaddington <nicolaaswaddington@gmail.com>
2017-03-11 11:44:17 +1300

    .

--- roomFindAdjacent.js
+++ roomFindAdjacent.js
@@ -1,7 +1,11 @@
 module.exports = function(p) {
 	var positions = this.lookForAtArea(LOOK_TERRAIN, p.y-1, p.x-1, p.y+1, p.x+1, true);
 	
-	console.log(JSON.stringify(positions));
+	var filtered = _.filter(positions, function(o) {
+		return o.terrain !== 'wall';
+	});
+	
+	console.log(JSON.stringify(filtered));
 	
 	return p;
 };
