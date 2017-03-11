1e770bf448518ada3ebaa9f39b054064f63d25a6
nickwaddington <nicolaaswaddington@gmail.com>
2017-03-11 13:48:41 +1300

    unassign job on death

--- main.js
+++ main.js
@@ -8,28 +8,28 @@ Room.prototype.initialise = require('roomInitialise');
 Room.prototype.findAdjacent = require('roomFindAdjacent');
 
 module.exports.loop = function () {
-    var timeCurrent = Game.time;
-    
-    //clear dead creeps memory
-    /*for(var name in Memory.creeps) {
+    for(var name in Memory.creeps) {
         if(!Game.creeps[name]) {
             delete Memory.creeps[name];
+            
+            for(var i in Memory.jobs) {
+            	if(Memory.jobs[i].assignedTo === name) {
+            		Memory.jobs[i].assignedTo = null;
+            	}
+            }
         }
-    }*/
+    }
     
     for(var currentRoom in Game.rooms) {
         var rm = Game.rooms[currentRoom];
-        
         rm.run();
     }
     
     for(var currentCreep in Game.creeps) {
         var crp = Game.creeps[currentCreep];
-        
         crp.run();
     }
     
     //print time an cpu usage
-    var cpuUsedThisTick = Game.cpu.getUsed()*100/Game.cpu.limit;
-    console.log("Time: " + timeCurrent + "  " + Math.round(cpuUsedThisTick) + "% CPU used");
+    console.log("Time: " + Game.time + "  " + Math.round(Game.cpu.getUsed()*100/Game.cpu.limit) + "% CPU used");
 };
