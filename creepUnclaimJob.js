f89dedc1841299bedcdee0fc55d3f903d5f203c8
nickwaddington <nicolaaswaddington@gmail.com>
2017-03-11 20:53:03 +1300

    Getting job amounts working

--- creepUnclaimJob.js
+++ creepUnclaimJob.js
@@ -3,6 +3,10 @@ module.exports = function() {
 		for(var j in Memory.jobs[i].assignedTo) {
 			if(Memory.jobs[i].assignedTo[j] === this.name) {
 				Memory.jobs[i].assignedTo.splice(j,1);
+				
+				if(Memory.jobs[i].type === 4) {
+					console.log(Game.getObjectById(Memory.jobs[i].location).energy)
+				}
 			}
 		}
 	}
