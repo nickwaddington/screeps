f8802ee61272bfbeb474e09b6dd25a051f4ede9f
nickwaddington <nicolaaswaddington@gmail.com>
2017-03-12 17:22:27 +1300

    only spawn if unclaimed jobs

--- roomRun.js
+++ roomRun.js
@@ -8,19 +8,27 @@ module.exports = function() {
 	
 	var spawn = this.find(FIND_MY_SPAWNS)[0];
 	
-	var status = spawn.createCreep([WORK,CARRY,MOVE], null, {
-    	action: 0,
-    	role: 'worker'
-    });
+	var unclaimedJob = _.find(Memory.jobs, function(o) {
+		return o.assignedTo.length === 0;
+	});
 	
-	if(typeof status === 'string') {
-		//Update job stuff here
-		if(!jobManager.updateJob(SPAWN_ENERGY_CAPACITY - spawn.energy, function(o) {
-			return o.location === spawn.id && o.type === 4;
-		})) {
-			jobManager.addJob(4, 0, 0, 0, SPAWN_ENERGY_CAPACITY - spawn.energy, spawn.id);
+	if(unclaimedJob) {
+		var status = spawn.createCreep([WORK,CARRY,MOVE], null, {
+	    	action: 0,
+	    	role: 'worker'
+	    });
+		
+		if(typeof status === 'string') {
+			//Update job stuff here
+			if(!jobManager.updateJob(SPAWN_ENERGY_CAPACITY - spawn.energy, function(o) {
+				return o.location === spawn.id && o.type === 4;
+			})) {
+				jobManager.addJob(4, 0, 0, 0, SPAWN_ENERGY_CAPACITY - spawn.energy, spawn.id);
+			}
 		}
 	}
+	
+	
 	
 	
 };
