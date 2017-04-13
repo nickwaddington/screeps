37dcfa277d4dae9314544f856cb9bdb5cc672340
nickwaddington <nicolaaswaddington@gmail.com>
2017-04-13 16:36:10 +1200

    Add files via upload

--- main.js
+++ main.js
@@ -11,19 +11,25 @@ module.exports.loop = function () {
     
     var rm = Game.rooms['sim'];
     var spawn = rm.find(FIND_MY_SPAWNS)[0];
-    
+    var rv = new RoomVisual(rm.name)
     var sources = rm.find(FIND_SOURCES_ACTIVE);
     var src1 = sources[0];
     var src2 = sources[3];
     
-    new RoomVisual(rm.name).rect(src1.pos.x - 1, src1.pos.y - 1, 2, 2);
-    new RoomVisual(rm.name).rect(src2.pos.x - 1, src2.pos.y - 1, 2, 2);
+    rv.rect(src1.pos.x - 1, src1.pos.y - 1, 2, 2);
+    rv.rect(src2.pos.x - 1, src2.pos.y - 1, 2, 2);
     
-    var src1spots = pathUtilities.findAdjacent(rm, src1.pos);
-    var src2spots = pathUtilities.findAdjacent(rm, src2.pos);
+    var plotPoints = []
     
-    new RoomVisual(rm.name).poly(src1spots, {stroke: '#0000ff', lineStyle: 'dotted'});
-    new RoomVisual(rm.name).poly(src2spots, {stroke: '#0000ff', lineStyle: 'dotted'});
+    plotPoints.push(pathUtilities.findAdjacent(rm, src1.pos));
+    plotPoints.push(pathUtilities.findAdjacent(rm, src2.pos));
+    
+    for(var i in plotPoints) {
+    	rv.circle(plotPoints[i], {radius: 0.5, stroke: 'blue'})
+    }
+    
+    rv.poly(src1spots, {stroke: '#0000ff', lineStyle: 'dotted'});
+    rv.poly(src2spots, {stroke: '#0000ff', lineStyle: 'dotted'});
     
     for(var currentCreep in Game.creeps) {
         var crp = Game.creeps[currentCreep];
@@ -31,5 +37,5 @@ module.exports.loop = function () {
     }
     
     
-    new RoomVisual(rm.name).text("Time: " + Game.time + "  " + Math.round(Game.cpu.getUsed()*100/Game.cpu.limit) + "% CPU used",2,2);
+    rv.text("Time: " + Game.time + "  " + Math.round(Game.cpu.getUsed()*100/Game.cpu.limit) + "% CPU used",2,2);
 };
