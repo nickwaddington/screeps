1a908f73432f215eb5ed13db675ef85c314f6310
nickwaddington <nicolaaswaddington@gmail.com>
2017-04-13 16:02:51 +1200

    Add files via upload

--- main.js
+++ main.js
@@ -13,13 +13,18 @@ module.exports.loop = function () {
     var rm = Game.rooms['sim'];
     var spawn = rm.find(FIND_MY_SPAWNS)[0];
     
+    var sources = rm.find(FIND_SOURCES_ACTIVE);
+    var src1 = sources[0];
+    var src2 = sources[1];
     
+    new RoomVisual(rm.name).rect(src1.pos.x - 1, src1.pos.y - 1, 3, 3);
+    new RoomVisual(rm.name).rect(src2.pos.x - 1, src2.pos.y - 1, 3, 3);
     
     for(var currentCreep in Game.creeps) {
         var crp = Game.creeps[currentCreep];
         
     }
     
-    //print time and cpu usage
-    new RoomVisual('sim').text("Time: " + Game.time + "  " + Math.round(Game.cpu.getUsed()*100/Game.cpu.limit) + "% CPU used",1,1);
+    
+    new RoomVisual(rm.name).text("Time: " + Game.time + "  " + Math.round(Game.cpu.getUsed()*100/Game.cpu.limit) + "% CPU used",2,2);
 };
