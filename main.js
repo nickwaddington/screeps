13873be3626b1ae46842649df2d562d890cae6c5
nickwaddington <nicolaaswaddington@gmail.com>
2017-04-13 15:47:41 +1200

    Add files via upload

--- main.js
+++ main.js
@@ -1,19 +1,7 @@
-Creep.prototype.run = require('creepRun');
 
-Room.prototype.findAdjacent = require('roomFindAdjacent');
-
-var calendar = require('calendarManager');
 var pathUtilities = require('pathUtilities');
 
 module.exports.loop = function () {
-	if(typeof Memory.calendar === 'undefined') {
-		Memory.calendar = [];
-		Memory.states = [];
-		Memory.energy = [];
-		Memory.info = [];
-		
-		calendar.add('spawn', Game.time, 'Spawn1', 0, [WORK, MOVE, CARRY]);
-	}
 	
     for(var name in Memory.creeps) {
         if(!Game.creeps[name]) {
@@ -22,14 +10,14 @@ module.exports.loop = function () {
         }
     }
     
-    //var rm = Game.rooms['sim'];
-    //var spawn = rm.find(FIND_MY_SPAWNS)[0];
+    var rm = Game.rooms['sim'];
+    var spawn = rm.find(FIND_MY_SPAWNS)[0];
     
-    calendar.runCurrentTick();
+    
     
     for(var currentCreep in Game.creeps) {
         var crp = Game.creeps[currentCreep];
-        crp.run();
+        
     }
     
     //print time and cpu usage
