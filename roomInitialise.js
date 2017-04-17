70cb4dd6bd6efd4089332c5c13cf6618be5c5d2b
nickwaddington <nicolaaswaddington@gmail.com>
2017-04-17 12:34:06 +1200

    Add files via upload

--- roomInitialise.js
+++ roomInitialise.js
@@ -4,10 +4,10 @@ module.exports = function() {
 	var spawnList = this.find(FIND_MY_SPAWNS);
 	
 	if(spawnList.length > 0) {
-		rm.memory.type = 'main';
+		this.memory.type = 'main';
 	}
 	else {
-		rm.memory.type = 'other';
+		this.memory.type = 'other';
 	}
 	
 	pathUtilities.initialisePaths(this);
